8eaf706f682593656f2b6ac510b96480c3301728
Olawale Oyeyipo <olaclassik@gmail.com>
2020-02-25 18:38:40 +0100

    Basic view now returns minimal HTML

--- mysite/tests.py
+++ mysite/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from mysite.views import landing_page
 
 
@@ -7,4 +8,12 @@ class LandingPageTest(TestCase):
     def test_root_url_resolves_to_landing_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, landing_page)
+
+    def test_landing_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = landing_page(request)
+        html = response.content.decode("utf8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>Oyeyipo Olawale</title>", html)
+        self.assertTrue(html.endswith("</html>"))
 
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def landing_page():
-    pass
+def landing_page(request):
+    return HttpResponse("<html><title>Oyeyipo Olawale</title></html>")

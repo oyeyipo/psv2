62b05b2d1daa740bbed6b600c4b1f10f31cbb948
Olawale Oyeyipo <olaclassik@gmail.com>
2020-02-25 17:43:49 +0100

    Add views and tests file for landing page with deliberately failing test

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_do_something(self):
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finished the test")
+
+    def test_can_navigate_to_my_work_section(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+print("Heello")
 

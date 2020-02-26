4237a6ae9d2f6035081efaf6392a981e79232e6a
Olawale Oyeyipo <olaclassik@gmail.com>
2020-02-26 21:48:00 +0100

    Refactor landing page view to use a template

--- mysite/tests.py
+++ mysite/tests.py
@@ -2,18 +2,11 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from mysite.views import landing_page
+from django.template.loader import render_to_string
 
 
 class LandingPageTest(TestCase):
-    def test_root_url_resolves_to_landing_page_view(self):
-        found = resolve("/")
-        self.assertEqual(found.func, landing_page)
-
     def test_landing_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = landing_page(request)
-        html = response.content.decode("utf8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>Oyeyipo Olawale</title>", html)
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "core/landing.html")
 
--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def landing_page(request):
-    return HttpResponse("<html><title>Oyeyipo Olawale</title></html>")
+    return render(request, "core/landing.html")
+

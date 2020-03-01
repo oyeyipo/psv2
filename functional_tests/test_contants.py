2edcd85e449365b9252ed05c499caadab7e732d3
Olawale Oyeyipo <olaclassik@gmail.com>
2020-03-01 14:23:11 +0100

    FT for landing page constants first cut

--- functional_tests/test_contants.py
+++ functional_tests/test_contants.py
@@ -1,9 +1,16 @@
 from .base import FunctionalTest
 
-class ConstantsTest(FunctionalTest):
 
+class ConstantsTest(FunctionalTest):
     def test_landing_page_header_contants(self):
         # Aduke heard about a software engineer and decides to
         # checkout his website homepage
+        self.browser.get(self.live_server_url)
         # she notices the title and header mentions "Software Engineer"
-        pass  
+        self.assertIn("Software Engineer", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("Software Engineer", header_text)
+
+        # [continued]
+        self.fail("Finish the test")
+

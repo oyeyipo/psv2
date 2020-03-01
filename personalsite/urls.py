73b83c56012b97ad09a5223307d759f8c0dc67b7
Olawale Oyeyipo <olaclassik@gmail.com>
2020-03-01 18:28:12 +0100

    static setup, wired in sass, update base template

--- personalsite/urls.py
+++ personalsite/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
-    path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="landing.html"), name="landing"),
+    path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

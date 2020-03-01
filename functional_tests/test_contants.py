b96bb9d16211e024d3a974961608471ad0fb1c78
Olawale Oyeyipo <olaclassik@gmail.com>
2020-03-01 14:10:52 +0100

    rename functional test and install black formatter

---
+++ functional_tests/test_contants.py
@@ -0,0 +1,9 @@
+from .base import FunctionalTest
+
+class ConstantsTest(FunctionalTest):
+
+    def test_landing_page_header_contants(self):
+        # Aduke heard about a software engineer and decides to
+        # checkout his website homepage
+        # she notices the title and header mentions "Software Engineer"
+        pass  

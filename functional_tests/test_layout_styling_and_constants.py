80a3e8c38aa8175ff844c46fcfdc9455d4213370
Olawale Oyeyipo <olaclassik@gmail.com>
2020-03-02 11:58:06 +0100

    styling smoke test; still not sure

--- functional_tests/test_layout_styling_and_constants.py
+++ functional_tests/test_layout_styling_and_constants.py
@@ -10,3 +10,17 @@ class ConstantsTest(FunctionalTest):
         self.assertIn("Software Engineer", self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn("Software Engineer", header_text)
+
+
+class LayoutAndStylingTest(FunctionalTest):
+    def test_layout_and_styling(self):
+        # Aduke goes to the landing page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        # she notice the h1 text is nicely centerd
+        header_textbox = self.browser.find_element_by_class_name("header_box")
+        self.assertAlmostEqual(
+            header_textbox.location["x"] + header_textbox.size["width"] / 2,
+            512,
+            delta=10,
+        )
